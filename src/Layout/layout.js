fa576eb315d0487a93bc66dc1683609d8635566e
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2024-07-10 15:48:23 +0530

    feat: Create main layout for webapp

---
+++ src/Layout/layout.js
@@ -0,0 +1,96 @@
+export const layoutConfig = {
+  header: {
+    component: [
+      {
+        name: "Header",
+        style: {
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#f0f0f0",
+        },
+      },
+    ],
+    style: {
+      width: "100%",
+      height: "10%",
+    },
+  },
+  dropdown: {
+    component: [
+      {
+        name: "Dropdown",
+        style: {
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#f0f0f0",
+        },
+      },
+    ],
+    style: {
+      width: "100%",
+      height: "10%",
+    },
+  },
+  lhs: {
+    component: [
+      {
+        name: "SidebarTop",
+        style: {
+          height: "50%",
+          backgroundColor: "#f0f0f0",
+        },
+      },
+      {
+        name: "SidebarBottom",
+        style: {
+          height: "50%",
+          backgroundColor: "#f0f0f0",
+        },
+      },
+    ],
+    style: {
+      width: "10%",
+      height: "100%",
+    },
+  },
+  center: {
+    component: [
+      {
+        name: "MapComponent",
+        style: {
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#f0f0f0",
+        },
+      },
+    ],
+    style: {
+      width: "80%",
+      height: "100%",
+    },
+  },
+  rhs: {
+    component: [
+      {
+        name: "ToolbarTop",
+        style: {
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#f0f0f0",
+        },
+      },
+      {
+        name: "ToolbarBottom",
+        style: {
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#f0f0f0",
+        },
+      },
+    ],
+    style: {
+      width: "10%",
+      height: "100%",
+    },
+  },
+};
