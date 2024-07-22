7b23012a3c7ae9f994e5cfb387978de2c0e19a91
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2024-07-22 13:54:25 +0530

    Add demo content for SideBar tabs

--- src/Components/LayerTab.jsx
+++ src/Components/LayerTab.jsx
@@ -2,8 +2,74 @@ import React from 'react'
 
 const LayerTab = () => {
   return (
-    <div>LayerTab</div>
-  )
+    <div>
+      <fieldset>
+        <legend className="sr-only">Checkboxes</legend>
+
+        <div className="space-y-2">
+          <label
+            htmlFor="Option1"
+            className="flex cursor-pointer items-start gap-4"
+          >
+            <div className="flex items-center">
+              &#8203;
+              <input
+                type="checkbox"
+                className="size-4 rounded border-gray-300"
+                id="Option1"
+              />
+            </div>
+
+            <div>
+              <strong className="font-medium text-gray-900">
+                {" "}
+                Layer 1{" "}
+              </strong>
+            </div>
+          </label>
+
+          <label
+            htmlFor="Option2"
+            className="flex cursor-pointer items-start gap-4"
+          >
+            <div className="flex items-center">
+              &#8203;
+              <input
+                type="checkbox"
+                className="size-4 rounded border-gray-300"
+                id="Option2"
+              />
+            </div>
+
+            <div>
+              <strong className="font-medium text-gray-900">
+                {" "}
+                Layer 2{" "}
+              </strong>
+            </div>
+          </label>
+
+          <label
+            htmlFor="Option3"
+            className="flex cursor-pointer items-start gap-4"
+          >
+            <div className="flex items-center">
+              &#8203;
+              <input
+                type="checkbox"
+                className="size-4 rounded border-gray-300"
+                id="Option3"
+              />
+            </div>
+
+            <div>
+              <strong className="font-medium text-gray-900"> Layer 3 </strong>
+            </div>
+          </label>
+        </div>
+      </fieldset>
+    </div>
+  );
 }
 
 export default LayerTab
